e5e8f01a1d85a9fe27117cf1b504f7644df9e8a3
ashes999 <ashes999@yahoo.com>
2014-01-10 22:48:24 -0500

    Added moving, colliding character

--- src/entities.js
+++ src/entities.js
@@ -1,15 +1,38 @@
 // A Tree is just an Actor with a certain color
 Crafty.c('Tree', {
 	init: function() {
-		this.requires('Actor, Color')
+		this.requires('Actor, Color, Solid')
 			.color('rgb(20, 125, 40)');
-	},
+	}
 });
 
 // A Bush is just an Actor with a certain color
 Crafty.c('Bush', {
 	init: function() {
-		this.requires('Actor, Color')
+		this.requires('Actor, Color, Solid')
 			.color('rgb(64, 215, 78)');
+	}
+});
+
+// This is the player-controlled character
+Crafty.c('Player', {
+	init: function() {
+		this.requires('Actor, Fourway, Color, Grid, Collision')
+			.fourway(4)
+			.color('rgb(80, 168, 215)')
+			.stopOnSolids();
 	},
+	
+	stopOnSolids: function() {
+		this.onHit('Solid', this.stopMovement);
+		return this;
+	},
+	
+	stopMovement: function() {
+		this._speed = 0;
+		if (this._movement) {
+			this.x -= this._movement.x;
+			this.y -= this._movement.y;
+		}
+	}
 });
--- src/game.js
+++ src/game.js
@@ -12,6 +12,11 @@ Game = {
 		}
 	},
 	
+	player_start: {
+		x: 5,
+		y: 5
+	},
+	
 	// The total width of the game screen. Since our grid takes up the entire screen
 	//  this is just the width of a tile times the width of the grid
 	width: function() {
@@ -27,8 +32,7 @@ Game = {
 	start: function() {
 		// Start crafty and set a background color so that we can see it's working
 		Crafty.init(Game.width(), Game.height());
-		Crafty.background('#dfb');
-		
+		Crafty.background('#dfb');		
 		Game.init_map();
 	},
 	
@@ -41,10 +45,12 @@ Game = {
 				if (at_edge) {
 					// Place a tree entity at the current tile
 					Crafty.e('Tree').move_on_grid(x, y);
-				} else if (Math.random() < 0.06) {
+				} else if (Math.random() < 0.06 && x != Game.player_start.x && y != Game.player_start.y) {
 					Crafty.e('Bush').move_on_grid(x, y);
 				}
 			}
 		}
+		
+		Crafty.e('Player').move_on_grid(Game.player_start.x, Game.player_start.y);
 	}
 }
