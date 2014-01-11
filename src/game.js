d71f289753cd64407a3ac59fbd8b3d5d6cc5f610
ashes999 <ashes999@yahoo.com>
2014-01-10 22:11:50 -0500

    Initial commit: Crafty.js

---
+++ src/game.js
@@ -0,0 +1,8 @@
+Game = {
+	// Initialize and start our game
+	start: function() {
+		// Start crafty and set a background color so that we can see it's working
+		Crafty.init(800, 600);
+		Crafty.background('#dfb');
+	}
+}
