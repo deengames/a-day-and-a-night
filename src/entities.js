c145939bd223edc8656dc3ebadeebe2a4698d5e5
ashes999 <ashes999@yahoo.com>
2014-01-16 18:23:03 -0500

    Part of #5: Bounce when you hit an object.
    - Fixed glitch with velocity being copied, not set
    - Added helper to create walk cycle from this style of image

--- src/entities.js
+++ src/entities.js
@@ -16,22 +16,24 @@ Crafty.c('Tree', {
 
 Crafty.c('Npc', {
 	init: function() {
-		var animationDuration = 500; //ms	
+		var animationDuration = 600; //ms	
 		
 		this.requires('Actor, Color, sprite_npc1, SpriteAnimation, Solid, Collision, Interactive')
 			.color('none')			
-			.reel('MovingDown', animationDuration, 0, 0, 2)
-			.reel('MovingLeft', animationDuration, 0, 1, 2)
-			.reel('MovingRight', animationDuration, 0, 2, 2)
-			.reel('MovingUp', animationDuration, 0, 3, 2);
+			.reel('MovingDown', animationDuration, getFramesForRow(0))
+			.reel('MovingLeft', animationDuration, getFramesForRow(1))
+			.reel('MovingRight', animationDuration, getFramesForRow(2))
+			.reel('MovingUp', animationDuration, getFramesForRow(3));
 		
 		this.bind('EnterFrame', this.moveToTarget);
-		this.velocity = { x: 32, y: 0 };
+		this.velocity = { x: 0, y: 100 };
 		
 		this.onHit('Solid', function(data) {
 			this.x = this.lastX;
-			this.y = this.lastY;
-			this.pauseAnimation();
+			this.y = this.lastY
+			
+			this.velocity.x *= -1;
+			this.velocity.y *= -1;
 		}, function() {
 			this.resumeAnimation();
 		});
@@ -47,14 +49,20 @@ Crafty.c('Npc', {
 		this.y += this.velocity.y * elapsedMs;
 		
 		// Possible direction change
-		if (this.lastVelocity != this.velocity) {
+		if (this.lastVelocity == null || this.lastVelocity.x != this.velocity.x || this.lastVelocity.y != this.velocity.y) {
 			// Big assumption: move in only one direction at a time
 			if (this.velocity.x != 0) {
-				this.animate(this.velocity.x < 0 ? 'MovingLeft' : 'MovingRight', -1);
+				this.animate(this.velocity.x < 0 ? 'MovingLeft' : 'MovingRight', -1);				
 			} else if (this.velocity.y != 0) {
-				this.animate(this.velocity.y < 0 ? 'MovingUp' : 'MovingDown', -1);
-			}			
-			this.lastVelocity = this.velocity;
+				this.animate(this.velocity.y < 0 ? 'MovingUp' : 'MovingDown', -1);				
+			}
+			
+			if (this.lastVelocity == null) {
+				this.lastVelocity = { x: 0, y: 0 };
+			}
+			
+			this.lastVelocity.x = this.velocity.x;
+			this.lastVelocity.y = this.velocity.y;
 		}
 	}
 });
@@ -62,7 +70,7 @@ Crafty.c('Npc', {
 // This is the player-controlled character
 Crafty.c('Player', {
 	init: function() {
-		var animationDuration = 200; //ms
+		var animationDuration = 400; //ms
 		
 		this.requires('Actor, Color, MoveAndCollide, sprite_player, SpriteAnimation, Solid')
 			.fourway(4)			
@@ -70,24 +78,34 @@ Crafty.c('Player', {
 			.stopOnSolids()
 			
 			// Four animations (one per direction). X, Y, frames (excluding first frame)
-			.reel('PlayerMovingDown', animationDuration, 0, 0, 2)
-			.reel('PlayerMovingLeft', animationDuration, 0, 1, 2)
-			.reel('PlayerMovingRight', animationDuration, 0, 2, 2)
-			.reel('PlayerMovingUp', animationDuration, 0, 3, 2);
+			.reel('MovingDown', animationDuration, getFramesForRow(0))
+			.reel('MovingLeft', animationDuration, getFramesForRow(1))
+			.reel('MovingRight', animationDuration, getFramesForRow(2))
+			.reel('MovingUp', animationDuration, getFramesForRow(3));
 			
 		// Change direction: tap into event		
 		this.bind('NewDirection', function(data) {
 			if (data.x > 0) {
-				this.animate('PlayerMovingRight', -1);
+				this.animate('MovingRight', -1);
 			} else if (data.x < 0) {
-				this.animate('PlayerMovingLeft', -1);
+				this.animate('MovingLeft', -1);
 			} else if (data.y > 0) {
-				this.animate('PlayerMovingDown', -1);
+				this.animate('MovingDown', -1);
 			} else if (data.y < 0) {
-				this.animate('PlayerMovingUp', -1);
+				this.animate('MovingUp', -1);
 			} else {
 				this.pauseAnimation();
 			}
 		});
 	}
 });
+
+function getFramesForRow(rowY) {
+	// Walk cycle with three frames, but four steps.
+	toReturn = [];
+	toReturn.push([0, rowY]);
+	toReturn.push([1, rowY]);
+	toReturn.push([2, rowY]);
+	toReturn.push([1, rowY]);
+	return toReturn;
+}
