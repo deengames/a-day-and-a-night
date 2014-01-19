762c668158c797085314ccc9aa54f69ae8bd3884
ashes999 <ashes999@yahoo.com>
2014-01-18 22:01:51 -0500

    Keep tests in separate colsures (no shared global scope).
    See end of:
    http://www.testdrivenjs.com/getting-started/introduction-to-tests-in-qunit/

--- tests/components.js
+++ tests/components.js
@@ -1,18 +1,22 @@
-module('Component Tests');
+(function() {
 
-test( "Grid's gridX and gridY get results of move(x, y)", function() {
-	var grid = Crafty.e('Grid');
-	grid.move(2, 7);
-	equal(grid.gridX(), 2);
-	equal(grid.gridY(), 7);
-});
+	module('Component Tests');
 
-test( "Interactive's interact invokes function from onInteract", function() {
-	var isSuccess = false;
-	e = Crafty.e('Interactive');
-	e.onInteract(function() {
-		isSuccess = true;
+	test( "Grid's gridX and gridY get results of move(x, y)", function() {
+		var grid = Crafty.e('Grid');
+		grid.move(2, 7);
+		equal(grid.gridX(), 2);
+		equal(grid.gridY(), 7);
 	});
-	e.interact();
-	equal(isSuccess, true);
-});
+
+	test( "Interactive's interact invokes function from onInteract", function() {
+		var isSuccess = false;
+		e = Crafty.e('Interactive');
+		e.onInteract(function() {
+			isSuccess = true;
+		});
+		e.interact();
+		equal(isSuccess, true);
+	});
+
+})();
--- tests/entities.js
+++ tests/entities.js
@@ -1,26 +1,30 @@
-module('Entity Tests');
+(function() {
 
-test("Wall is solid", function() {
-	var wall = Crafty.e('Wall');
-	equal(wall.has('Solid'), true);
-});
+	module('Entity Tests');
 
-test("Tree is solid", function() {
-	var tree = Crafty.e('Tree');
-	equal(tree.has('Solid'), true);
-});
+	test("Wall is solid", function() {
+		var wall = Crafty.e('Wall');
+		equal(wall.has('Solid'), true);
+	});
 
-test("NpcBase has default sprite", function() {
-	var base = Crafty.e("NpcBase");
-	equal(base.has('default_sprite'), true);
-});
+	test("Tree is solid", function() {
+		var tree = Crafty.e('Tree');
+		equal(tree.has('Solid'), true);
+	});
 
-test("NpcBase moveOnVelocity moves according to velocity", function() {
-	var base = Crafty.e("NpcBase");
-	base.setVelocity(200, 100);
-	base.move(0, 0);
-	base.moveOnVelocity({ dt: 100 });
-	ok(base.x > 0);
-	ok(base.y > 0);
-	ok(base.x == 2 * base.y);
-});
+	test("NpcBase has default sprite", function() {
+		var base = Crafty.e("NpcBase");
+		equal(base.has('default_sprite'), true);
+	});
+
+	test("NpcBase moveOnVelocity moves according to velocity", function() {
+		var base = Crafty.e("NpcBase");
+		base.setVelocity(200, 100);
+		base.move(0, 0);
+		base.moveOnVelocity({ dt: 100 });
+		ok(base.x > 0);
+		ok(base.y > 0);
+		ok(base.x == 2 * base.y);
+	});
+
+})();
