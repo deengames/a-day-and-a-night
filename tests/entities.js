f14f93622f65f2ab3fa989f195f99735e2e3a0ed
ashes999 <ashes999@yahoo.com>
2014-01-18 21:57:54 -0500

    Resolved #8: finished entity testing
    Added scenes and load scene to initialize sprite constants.

--- tests/entities.js
+++ tests/entities.js
@@ -1,11 +1,26 @@
 module('Entity Tests');
 
-test( "Tree is solid", function() {
+test("Wall is solid", function() {
+	var wall = Crafty.e('Wall');
+	equal(wall.has('Solid'), true);
+});
+
+test("Tree is solid", function() {
 	var tree = Crafty.e('Tree');
 	equal(tree.has('Solid'), true);
 });
 
-test( "Wall is solid", function() {
-	var wall = Crafty.e('Wall');
-	equal(wall.has('Solid'), true);
+test("NpcBase has default sprite", function() {
+	var base = Crafty.e("NpcBase");
+	equal(base.has('default_sprite'), true);
+});
+
+test("NpcBase moveOnVelocity moves according to velocity", function() {
+	var base = Crafty.e("NpcBase");
+	base.setVelocity(200, 100);
+	base.move(0, 0);
+	base.moveOnVelocity({ dt: 100 });
+	ok(base.x > 0);
+	ok(base.y > 0);
+	ok(base.x > base.y);
 });
