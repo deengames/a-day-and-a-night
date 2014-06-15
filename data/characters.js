9dd90159d430df5be684b5b7b80e8318a8458e66
ashes999 <ashes999@yahoo.com>
2014-06-15 00:24:32 -0400

    Delete everything. We're starting over.

--- data/characters.js
+++
@@ -1,7 +0,0 @@
-function characters() {
-	var characters = {
-		'hero': { name: 'Saif', avatar: 'assets/images/main-character.png' },
-		'woman': { 'name': 'Woman' }
-	};	
-	return characters;
-}
--- data/maps/masjid_tileset.js
+++
@@ -1,5 +0,0 @@
-function masjid_tileset() {
-	var tileset = {"width":8,"height":7,"tilesets":["assets/images/indoors.png"],"ignore":[],"above":[33],"solid":[1,2,5,7,8,33,41]};
-
-	return tileset;
-}
