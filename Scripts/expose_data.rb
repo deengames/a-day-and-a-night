5bb77a3f69bd5406b9c83d7f3428416bf844d5e7
ashes999 <ashes999@yahoo.com>
2014-06-18 23:49:29 -0400

    Updated expose_data script; it's 80% useful.

--- Scripts/expose_data.rb
+++ Scripts/expose_data.rb
@@ -7,9 +7,35 @@
 # Automatically converts .rvdata2 files into plain text, so you can view
 # them and diff them and manage changes to your game more easily.
 #========================================================================
+def simple_print(obj)
+	if obj.is_a?(Hash)
+		str = "{\n"
+		obj.each do |k, v|		
+			str += "\t#{k} => #{v.inspect}\n"
+		end
+		str += "}"
+	elsif obj.is_a?(Array)
+		return obj.to_s
+	else
+		str = "{\n"
+			obj.instance_variables.each do |i|
+				value = obj.instance_variable_get(i)			
+				value = 'nil' if value.nil? || value == ''
+				value = simple_print(value) if value.is_a?(Table)
+				str += "\t#{i} => #{value.inspect}\n"
+			end
+		str += "}"
+	end
+end
+
 files = Dir.glob('Data/*.rvdata2')
 files.each do |f|
     binary = File.binread(f)
     object = Marshal.load(binary)
-	File.open(f.sub('.rvdata2', '-data.txt'), 'w') { |out| out.write(object) }
+	# Normal: object
+	# Classes without to_s: object.inspect	
+	#output = object.inspect
+	output = simple_print(object)
+	File.open(f.sub('.rvdata2', '-data.txt'), 'w') { |out| out.write(output) }
 end
+
