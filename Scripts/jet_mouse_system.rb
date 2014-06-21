252419b096946bf501fa655df23aa44f00e28dad
ashes999 <ashes999@yahoo.com>
2014-06-21 16:49:06 -0400

    Gave up on mouse outline and just removed it. Also added character name to leave-town event.

--- Scripts/jet_mouse_system.rb
+++ Scripts/jet_mouse_system.rb
@@ -113,7 +113,7 @@ module Jet
     CHECK_FOR_MOVES = true
     
     # Would you like a black box to outline the exact tile the mouse is over?
-    DEV_OUTLINE = true
+    DEV_OUTLINE = false
     
   end
   
@@ -332,7 +332,7 @@ module Mouse
       self.visible = !$game_switches[Jet::MouseSystem::TURN_MOUSE_OFF_SWITCH]
       if !@outline.nil?
         @outline.visible = SceneManager.scene_is?(Scene_Map)
-        @outline.x = (Mouse.grid[0] * 32) - 16
+        @outline.x = Mouse.grid[0] * 32
         @outline.y = [Mouse.grid[1] * 32, 0].max
       end
     end
