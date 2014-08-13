bb1b5535366d92baaf7a96c0f35d5b89b3bfcb0b
ashes999 <ashes999@yahoo.com>
2014-08-13 05:57:51 -0400

    Added plant and cackle to apothecary map #133

---
+++ clean-up.rb
@@ -0,0 +1,11 @@
+puts 'Cleaning up files for release ...'
+`rm Audio/SE/*.wav`
+`rm Audio/SE/*.mp3`
+`rm Graphics/Characters/*.xcf`
+`rm -rf old-assets`
+`rm .gitignore`
+`rm .git`
+`rm README.md`
+`rm Save*.rvdata2`
+`rm undo.sh`
+`rm clean-up.rb`
